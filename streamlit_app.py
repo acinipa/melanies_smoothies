d9f1ca76fcf5595d9c2121cf3a1130838f3b9a8b
acinipa <Alexandru.Cinipa@endava.com>
2026-01-03 00:34:16 +0200

    Remove session variable assignment

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,6 @@ st.write(
 name_on_order = st.text_input("Name of Smoothie:")
 st.write("The name of your Smoothie will be:", name_on_order)
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
