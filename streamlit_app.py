b7911e134e35371a6533d997fbf8681d04fe991d
acinipa <Alexandru.Cinipa@endava.com>
2026-01-03 00:47:42 +0200

    Remove Snowflake session and add API call
    Removed Snowflake connection and added API call for watermelon.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,6 @@ ingredients_list = st.multiselect(
     ,max_selections=5
 )
 
-cnx = st.connection("snowflake")
-session = cnx.session()
 
 if ingredients_list:
     ingredients_string = ''
@@ -43,7 +41,12 @@ if ingredients_list:
     # st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
 
-    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+      
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
